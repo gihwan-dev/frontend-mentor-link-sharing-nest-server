03b122ea7c1e0ebb7cf4dd282987b841833c16ab
최기환 <rlghks9806@gmail.com>
2023-08-02 11:53:47 +0900

    회원가입 요청 api 개발 완료 테스트 해봐야함

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  async create(@Body() createAuthDto: CreateAuthDto) {
-    console.log(createAuthDto);
-    this.authService.create(createAuthDto);
+  async create(
+    @Body() createAuthDto: CreateAuthDto,
+    @Res() response: Response,
+  ) {
+    const result = await this.authService.create(createAuthDto);
+
+    return response.status(result.statusCode).json({ message: result.message });
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { CreateAuthDto, CreateAuthResponseDto } from './dto/create-auth.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Auth } from './entities/auth.entity';
 import { Repository } from 'typeorm';
+import { hashPassword } from './lib/hash';
 
 @Injectable()
 export class AuthService {
@@ -10,19 +11,42 @@ export class AuthService {
     @InjectRepository(Auth) private readonly authRepository: Repository<Auth>,
   ) {}
   async create(createAuthDto: CreateAuthDto): Promise<CreateAuthResponseDto> {
-    const isExisting = await this.authRepository.findOne({
-      where: {
-        email: createAuthDto.email,
-      },
-    });
+    try {
+      const isExisting = await this.authRepository.findOne({
+        where: {
+          email: createAuthDto.email,
+        },
+      });
 
-    if (isExisting) {
+      if (isExisting) {
+        return {
+          statusCode: HttpStatus.CONFLICT,
+          message: '이미 존재하는 이메일 입니다.',
+        };
+      }
+
+      const hashedAuth = { ...createAuthDto };
+
+      hashedAuth.password = await hashPassword(createAuthDto.password);
+
+      const createResult = await this.authRepository.create(hashedAuth);
+
+      if (!createResult) {
+        return {
+          statusCode: HttpStatus.FORBIDDEN,
+          message: '계정을 생성하는데 실패했습니다. 다시 시도해 주세요.',
+        };
+      }
       return {
-        statusCode: HttpStatus.CONFLICT,
-        message: '이미 존재하는 이메일 입니다.',
+        statusCode: HttpStatus.CREATED,
+        message: '성공적으로 계정을 생성했습니다.',
+      };
+    } catch (e) {
+      return {
+        statusCode: HttpStatus.FORBIDDEN,
+        message:
+          e.message ?? '계정을 생성하는데 실패했습니다. 다시 시도해 주세요.',
       };
     }
-
-    const createResult = await this.authRepository.create(createAuthDto);
   }
 }
