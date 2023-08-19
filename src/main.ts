a8302f1cffa10d77e2c1811120f1b1ccc52f87ee
최기환 <rlghks9806@gmail.com>
2023-08-20 02:46:33 +0900

    1

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  });
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: process.env.CLIENT_URL,
+  // });
   await app.listen(8000);
 }
 
