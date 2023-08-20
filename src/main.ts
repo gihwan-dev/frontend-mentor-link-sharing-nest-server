de323140d857f9a46f03d9a9af021ce5377a32d7
최기환 <rlghks9806@gmail.com>
2023-08-21 01:27:03 +0900

    123159102

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,7 @@ async function bootstrap() {
       '*',
     ],
   });
-  // app.use(cookieParser());
+  app.use(cookieParser());
   await app.listen(8000);
 }
 
