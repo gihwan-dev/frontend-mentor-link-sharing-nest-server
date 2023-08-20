824beddca145e27d7de7f68b398a56226971d288
최기환 <rlghks9806@gmail.com>
2023-08-20 21:02:46 +0900

    cors8

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -48,7 +48,7 @@ export class AuthController {
         .cookie('frontend-mentor-link-sharing', result.access_token, {
           maxAge: 60 * 60 * 1000,
           secure: true,
-          path: '/',
+          sameSite: 'none',
           domain: 'frontend-mentor-link-sharing-next-front.vercel.app',
         })
         .status(result.statusCode)
