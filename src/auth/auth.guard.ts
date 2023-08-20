30ebc47e45b0da7dcf7a7b960e71bb353e91f9d6
최기환 <rlghks9806@gmail.com>
2023-08-21 01:53:50 +0900

    cookie cookie cookie

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,6 +7,7 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { signedCookie, signedCookies } from 'cookie-parser';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -17,7 +18,7 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(request.headers);
+    console.log(request);
     const token = this.extractTokenFromHeader(request);
     if (!token) {
       throw new UnauthorizedException();
@@ -34,6 +35,9 @@ export class AuthGuard implements CanActivate {
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
+    console.log(request.cookies);
+    console.log(signedCookie);
+    console.log(signedCookies);
     return request.cookies['frontend-mentor-link-sharing'];
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     origin: [
       'https://frontend-mentor-link-sharing-next-front.vercel.app',
       'http://localhost:3000',
-      '*',
+      'https://https://frontend-mentor-link-sharing-nest-server.vercel.app',
     ],
   });
   app.use(cookieParser());
