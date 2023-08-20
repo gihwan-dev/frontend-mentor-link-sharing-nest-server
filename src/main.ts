e77408968cd7f99e4e114032ef8ba86768369cda
최기환 <rlghks9806@gmail.com>
2023-08-21 01:22:51 +0900

    how to get cookie?

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: [
-      'X-CSRF-Token',
-      'X-Requested-With, Accept',
-      'Accept-Version',
-      'Content-Length',
-      'Content-MD5',
-      'Content-Type',
-      'Date',
-      'X-Api-Version',
-    ],
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     origin: [
