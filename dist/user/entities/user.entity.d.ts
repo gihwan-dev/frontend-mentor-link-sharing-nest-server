bfb4526f09f72591df40fb5c08ee40114c61cede
최기환 <rlghks9806@gmail.com>
2023-08-20 20:29:02 +0900

    cors3

--- dist/user/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Platforms } from '../../platform/entities/platform.entity';
-export declare class Users {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    contactEmail: string;
-    platforms: Platforms[];
-}
--- dist/user/lib/image-lib.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=image-lib.js.map
