ebe305665e4a0c3d0d9f330751cddb82617df1c2
최기환 <rlghks9806@gmail.com>
2023-08-21 09:08:01 +0900

    move server to azure

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,8 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true,
-    origin: [
-      'https://frontend-mentor-link-sharing-next-front.vercel.app',
-      'http://localhost:3000',
-    ],
-  });
   app.use(cookieParser());
-  await app.listen(process.env.PORT || 80);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
