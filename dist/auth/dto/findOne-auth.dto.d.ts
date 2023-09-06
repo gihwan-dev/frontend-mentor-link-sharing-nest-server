16e7d85313ee40051e2ce88aaa381c3e11765920
최기환 <rlghks9806@gmail.com>
2023-09-06 15:36:24 +0900

    1

--- dist/auth/dto/findOne-auth.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FindOneAuthDto {
-    email: string;
-    password: string;
-}
-export interface UpdateAuthResponseDto {
-    message: string;
-    statusCode: number;
-    access_token: string | null;
-}
