f233d6f32c812a6ac46367f986e526a95202cbb7
최기환 <rlghks3004@gmail.com>
2023-12-07 18:57:54 +0900

    .

--- dist/platform/platform.controller.d.ts
+++ dist/platform/platform.controller.d.ts
@@ -4,9 +4,9 @@ import { UpdatePlatformDto } from './dto/update-platform.dto';
 export declare class PlatformController {
     private readonly platformService;
     constructor(platformService: PlatformService);
-    findMany(req: any): Promise<{
+    findMany(req: any): Promise<InternalServerErrorException | {
         platforms: import("./entities/platform.entity").Platforms[];
-    } | InternalServerErrorException>;
+    }>;
     update(req: any, updatePlatformDto: UpdatePlatformDto): Promise<{
         message: string;
     }>;
