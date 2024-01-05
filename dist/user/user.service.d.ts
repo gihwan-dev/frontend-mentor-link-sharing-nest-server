48bcba3ee77e59b7db41d033acab9be1c18b0539
최기환 <rlghks3004@gmail.com>
2024-01-05 14:47:34 +0900

    프로덕션 모드 사용

--- dist/user/user.service.d.ts
+++ dist/user/user.service.d.ts
@@ -1,12 +1,10 @@
-/// <reference types="qs" />
-/// <reference types="express" />
 import { Repository } from 'typeorm';
 import { CreateUserDto, CreateUserResponseDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Users } from './entities/user.entity';
 export declare class UserService {
     private readonly userRepository;
-    uploadToS3: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
+    uploadToS3: any;
     private readonly uploadPath;
     constructor(userRepository: Repository<Users>);
     findOne(email: string): Promise<{
