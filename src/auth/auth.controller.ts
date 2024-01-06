f9466be41cac57fd316173fee0882cedfa0836ca
최기환 <rlghks3004@gmail.com>
2024-01-06 19:09:02 +0900

    서브 도메인 도메인 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -47,7 +47,7 @@ export class AuthController {
           path: '/',
           sameSite: "none",
           secure: true,
-          domain: 'azurewebsites.net',
+          domain: '.azurewebsites.net',
           httpOnly: true,
         })
         .status(result.statusCode)
